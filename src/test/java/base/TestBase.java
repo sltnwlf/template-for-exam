5fde02a3190d3e3064ac7cdb2e4d1253a3234590
sltnwlf <farkas.zoli.90@gmail.com>
2022-07-18 16:28:53 +0200

    headless done

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -27,7 +27,7 @@ public abstract class TestBase {
         options.addArguments("--disable-notifications");
         options.addArguments("−−incognito");
         options.addArguments("--disable-extensions");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--window-size=1920, 1080");
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
